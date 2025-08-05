3b07df75b04b4d72db2545f7cb6d840f82f0b21e
niltrip <niltrip@web.de>
2025-08-05 22:18:38 +0200

    change domain

--- custom_components/powerocean/const.py
+++ custom_components/powerocean/const.py
@@ -4,7 +4,7 @@ import logging
 
 from homeassistant.const import Platform
 
-DOMAIN = "ecoflow"
+DOMAIN = "powerocean"
 ISSUE_URL = "https://github.com/niltrip/powerocean/issues"
 ISSUE_URL_ERROR_MESSAGE = " Please log any issues here: " + ISSUE_URL
 LENGTH_BATTERIE_SN = 12  # Length of the battery serial number to identify battery data
